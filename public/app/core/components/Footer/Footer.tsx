fd84ee70646fd5cd2f0dfa7ba2818c450b0c5b4f
Ajith Ravindranathan <ajith.ravindranathan@smarthub.ai>
2020-11-02 19:57:27 +0530

    Redoing Dayanand's changes on the forked repo

--- public/app/core/components/Footer/Footer.tsx
+++ public/app/core/components/Footer/Footer.tsx
@@ -10,26 +10,7 @@ export interface FooterLink {
 }
 
 export let getFooterLinks = (): FooterLink[] => {
-  return [
-    {
-      text: 'Documentation',
-      icon: 'document-info',
-      url: 'https://grafana.com/docs/grafana/latest/?utm_source=grafana_footer',
-      target: '_blank',
-    },
-    {
-      text: 'Support',
-      icon: 'question-circle',
-      url: 'https://grafana.com/products/enterprise/?utm_source=grafana_footer',
-      target: '_blank',
-    },
-    {
-      text: 'Community',
-      icon: 'comments-alt',
-      url: 'https://community.grafana.com/?utm_source=grafana_footer',
-      target: '_blank',
-    },
-  ];
+  return [];
 };
 
 export let getVersionLinks = (): FooterLink[] => {
