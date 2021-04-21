e2cfcf3f09e17f40e624b527880f3ba34e2b163a
Santosh Parashetti <santosh.parashetti@smarthub.ai>
2021-04-21 14:13:07 +0530

    Resolve cherry-pick conflicts

--- public/app/core/components/sidemenu/SideMenu.tsx
+++ public/app/core/components/sidemenu/SideMenu.tsx
@@ -2,12 +2,8 @@ import React, { PureComponent } from 'react';
 import appEvents from '../../app_events';
 import TopSection from './TopSection';
 import BottomSection from './BottomSection';
-import config from 'app/core/config';
 import { CoreEvents } from 'app/types';
-import { Branding } from 'app/core/components/Branding/Branding';
 import { Icon } from '@grafana/ui';
-
-const homeUrl = config.appSubUrl || '/';
 
 export class SideMenu extends PureComponent {
   toggleSideMenuSmallBreakpoint = () => {
@@ -16,9 +12,6 @@ export class SideMenu extends PureComponent {
 
   render() {
     return [
-      <a href={homeUrl} className="sidemenu__logo" key="logo">
-        <Branding.MenuLogo />
-      </a>,
       <div className="sidemenu__logo_small_breakpoint" onClick={this.toggleSideMenuSmallBreakpoint} key="hamburger">
         <Icon name="bars" size="xl" />
         <span className="sidemenu__close">
