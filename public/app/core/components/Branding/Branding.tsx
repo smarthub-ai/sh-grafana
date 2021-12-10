7df07e846bbaa311ea70cdfc6a52a6f19ad7f2a1
santosh-at-smarthub <santosh.parashetti@smarthub.ai>
2021-12-10 10:17:23 +0530

    Apply rebrand changes

--- public/app/core/components/Branding/Branding.tsx
+++ public/app/core/components/Branding/Branding.tsx
@@ -9,7 +9,7 @@ export interface BrandComponentProps {
 }
 
 const LoginLogo: FC<BrandComponentProps> = ({ className }) => {
-  return <img className={className} src="public/img/grafana_icon.svg" alt="Grafana" />;
+  return <img className={className} src="public/img/smarthub_icon.png" alt="SmartHub.ai" />;
 };
 
 const LoginBackground: FC<BrandComponentProps> = ({ className, children }) => {
@@ -23,7 +23,7 @@ const LoginBackground: FC<BrandComponentProps> = ({ className, children }) => {
       right: 0;
       bottom: 0;
       top: 0;
-      background: url(public/img/g8_login_${theme.isDark ? 'dark' : 'light'}.svg);
+      background: url(public/img/smarthub_bg.svg);
       background-position: top center;
       background-size: auto;
       background-repeat: no-repeat;
@@ -42,7 +42,7 @@ const LoginBackground: FC<BrandComponentProps> = ({ className, children }) => {
 };
 
 const MenuLogo: FC<BrandComponentProps> = ({ className }) => {
-  return <img className={className} src="public/img/grafana_icon.svg" alt="Grafana" />;
+  return <img className={className} src="public/img/smarthub_icon.png" alt="SmartHub.ai" />;
 };
 
 const LoginBoxBackground = () => {
@@ -58,8 +58,8 @@ export class Branding {
   static LoginBackground = LoginBackground;
   static MenuLogo = MenuLogo;
   static LoginBoxBackground = LoginBoxBackground;
-  static AppTitle = 'Grafana';
-  static LoginTitle = 'Welcome to Grafana';
+  static AppTitle = 'SmartHub.ai';
+  static LoginTitle = 'Welcome to SmartHub.ai';
   static GetLoginSubTitle = (): null | string => {
     return null;
   };
